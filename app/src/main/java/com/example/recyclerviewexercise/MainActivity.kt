99befd37880f573adccc75c985c49f709798a502
ReneSoAr <renejsosaarana@gmail.com>
2024-03-05 16:07:20 -0500

    recycler view introduction

---
+++ app/src/main/java/com/example/recyclerviewexercise/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.recyclerviewexercise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.recyclerviewexercise.ui.theme.RecyclerViewExerciseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RecyclerViewExerciseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SimpleRecyclerView()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SimpleRecyclerView() {
+    var names = listOf("Roberto","Samuel","Primitivo", "Jack")
+    LazyColumn {
+        items(names){Text(text = "estoy follando con $names")}
+
+    }
+}
