c22ee3147349fb29710fb52614154924d225c561
ReneSoAr <renejsosaarana@gmail.com>
2024-03-12 18:05:56 -0500

    recycler view terminado 2

--- app/src/main/java/com/example/recyclerviewexercise/SuperHeroView.kt
+++ app/src/main/java/com/example/recyclerviewexercise/SuperHeroView.kt
@@ -6,10 +6,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -38,17 +42,38 @@ fun SuperHeroView() {
 
 }
 
+@Composable
+fun SuperHeroGridView() {
+    val context = LocalContext.current
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(vertical = 4.dp, horizontal = 6.dp),
+        horizontalArrangement = Arrangement.spacedBy(space = 6.dp),
+        verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+    ) {
+        items(getSuperHeroes()) { superhero ->
+            ItemHero(superhero = superhero)
+            {
+                Toast.makeText(context, it.superHeroName, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}
+
 //Encargado de pintar el item
 @Composable
 fun ItemHero(superhero: SuperHero, onItemSelected:(SuperHero)->Unit) {
-    Card(border = BorderStroke(2.dp, Color.Red), modifier = Modifier.width(200.dp).clickable { onItemSelected(superhero) }) {
+    Card(border = BorderStroke(2.dp, Color.Red), modifier = Modifier
+        .width(200.dp)
+        .clickable { onItemSelected(superhero) }) {
         Column() {
             Image(
                 painter = painterResource(id = superhero.photo),
                 contentDescription = "super hero avatar",
                 modifier = Modifier.fillMaxWidth(),
                 contentScale = ContentScale.Crop)
-            
+
             Text(text = superhero.superHeroName, modifier = Modifier.align(Alignment.CenterHorizontally))
             Text(text = superhero.realName, modifier = Modifier.align(Alignment.CenterHorizontally), fontSize = 12.sp)
             Text(text = superhero.publisher, fontSize = 10.sp, modifier = Modifier.align(Alignment.End))
